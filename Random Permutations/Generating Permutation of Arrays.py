95dc88689477aa1d3d9960e8ba9b7768a60bc40e
Irfan <irfanpratamaallabout23@gmail.com>
2025-07-25 21:36:54 +0700

    Add examples for generating permutations of arrays using NumPy

---
+++ Random Permutations/Generating Permutation of Arrays.py
@@ -0,0 +1,10 @@
+from numpy import random
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(random.permutation(arr))
+
+"""
+permutasi itu sama aja kayak shufele cuma dia itu tidak mengubah array assli
+"""
