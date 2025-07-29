851ed1ea30e8163a42fa6118683e7f1497dc6b50
Irfan <irfanpratamaallabout23@gmail.com>
2025-07-29 22:41:05 +0700

    implementation for visualizing uniform distribution and provide an introduction to the concept

---
+++ Uniform Distribution/Visualization of Uniform Distribution.py
@@ -0,0 +1,17 @@
+"""di dalam sebauah method uniform terdapad 3 paramater 
+
+ada tiga low,high dan size
+low dan hig digunakan untuk pembatas sebuah nilai 
+
+"""
+
+
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+sns.displot(random.uniform(size=1000), )
+
+plt.show()
